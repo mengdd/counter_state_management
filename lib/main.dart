c165735c29247500e6462cfaaa7e1e8e2508fb5a
Dandan Meng <mengdandanno1@163.com>
2020-03-19 16:46:14 +0800

    MyHomePage could be stateless now

--- lib/main.dart
+++ lib/main.dart
@@ -20,21 +20,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
